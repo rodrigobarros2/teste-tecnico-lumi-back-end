9e76ef8199d46d41a3cd2b7895aba2b47d1b44f7
Rodrigo Barros <rodrigobarros110@hotmail.com>
2024-05-22 14:27:20 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ app.post("/upload", upload.single("file"), async (req, res) => {
     const allInformationTicket = await pdfParse(dataBuffer);
     const ticketText = allInformationTicket.text.split("\n").filter((v) => v.trim());
 
-    const objeto = {
+    const extractedData = {
       customerNumber: "",
       referenceMonth: "",
       electricity: [{}],
@@ -27,42 +27,33 @@ app.post("/upload", upload.single("file"), async (req, res) => {
       contributionPublicLighting: "",
     };
 
-    function extractNumbers(value: string) {
+    function isNumber(value: string) {
       const normalizedValue = parseFloat(value.replace(",", "."));
       return !isNaN(normalizedValue) && value.trim() !== "";
     }
-
-    const formatData = (index: string[]) =>
-      index.filter(extractNumbers).map((value, index) => {
-        const valueInformation = ["quantidade", "preco", "valor", "tarifa"];
-        const key = valueInformation[index];
-        return { [key]: value };
-      });
 
     function extractNumber(str: string) {
       const match = str.match(/[\d,]+/);
       return match ? match[0] : "";
     }
 
+    const formatData = (index: string[]) =>
+      index.filter(isNumber).map((value, index) => {
+        const valueInformation = ["quantity", "price", "value", "tariff"];
+        const key = valueInformation[index];
+        return { [key]: value };
+      });
+
     const customerIndex = ticketText.findIndex((item) => item.includes("Nº DO CLIENTE"));
     const referentIndex = ticketText.findIndex((item) => item.includes("Referente a"));
     const electricityIndex = ticketText.findIndex((item) => item.includes("Energia ElétricakWh"));
 
-    objeto.customerNumber = ticketText[customerIndex + 1].split(/\s+/).slice(1, 2).toString();
-    objeto.referenceMonth = ticketText[referentIndex + 1].split(/\s+/)[1];
-    objeto.electricity = formatData(ticketText[electricityIndex].split(/\s+/));
-    objeto.injectedEnergy = formatData(ticketText[electricityIndex + 1].split(/\s+/));
-    objeto.compensatedEnergy = formatData(ticketText[electricityIndex + 2].split(/\s+/));
-    objeto.contributionPublicLighting = extractNumber(ticketText[electricityIndex + 3].toString());
-
-    const extractedData = {
-      customerNumber: objeto.customerNumber,
-      referenceMonth: objeto.referenceMonth,
-      electricity: objeto.electricity,
-      injectedEnergy: objeto.injectedEnergy,
-      compensatedEnergy: objeto.compensatedEnergy,
-      contributionPublicLighting: objeto.contributionPublicLighting,
-    };
+    extractedData.customerNumber = ticketText[customerIndex + 1].split(/\s+/).slice(1, 2).toString();
+    extractedData.referenceMonth = ticketText[referentIndex + 1].split(/\s+/)[1];
+    extractedData.electricity = formatData(ticketText[electricityIndex].split(/\s+/));
+    extractedData.injectedEnergy = formatData(ticketText[electricityIndex + 1].split(/\s+/));
+    extractedData.compensatedEnergy = formatData(ticketText[electricityIndex + 2].split(/\s+/));
+    extractedData.contributionPublicLighting = extractNumber(ticketText[electricityIndex + 3].toString());
 
     const user = await prisma.user.create({
       data: extractedData,
