2af55a2e9803abc0c66f362d591602b9f71dc7de
Rodrigo Barros <rodrigobarros110@hotmail.com>
2024-05-25 16:58:04 -0300

    feat: create variables

--- src/server.ts
+++ src/server.ts
@@ -158,6 +158,8 @@ app.get("/download/:id", async (req, res) => {
   }
 });
 
-app.listen(3333, () => {
-  console.log("Server started on http://localhost:3333");
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => {
+  console.log(`Server started on ${port}!`);
 });
